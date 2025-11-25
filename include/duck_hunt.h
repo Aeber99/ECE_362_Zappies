2f18fe0ff10e9d26e7d99a94624fbe83f2cccca8
maryjoanfrank <maryjoanfrank@gmail.com>
2025-11-25 14:16:31 -0500

    cleaned up main

--- include/duck_hunt.h
+++ include/duck_hunt.h
@@ -3,7 +3,34 @@
 
 #include "vga16_graphics_v2.h"
 
-void drawDuck(int x, int y, char color);
+// Constants
+extern const int DUCK_W;
+extern const int DUCK_H;
+extern const int STEP;
+extern const int PIECE_SIZE;
+#define MAX_PARTICLES 400
+
+// Duck state
+extern int duck_x;
+extern int duck_y;
+extern int duck_alive;
+
+// Particle arrays
+extern int part_x[];
+extern int part_y[];
+extern int part_vx[];
+extern int part_vy[];
+extern char part_color[];
+extern int part_alive[];
+extern int n;
+
+// Function declarations
+void drawDuck(int x, int y);
 void eraseDuck(int x, int y);
+int isShotInDuck(int sx, int sy);
+void shatterDuck(void);
+void handleShot(int sx, int sy);
+void spawnRandomDuck(void);
+void initDuckHunt(void);
 
 #endif // DUCK_HUNT_H
